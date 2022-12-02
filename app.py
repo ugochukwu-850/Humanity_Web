ad716d2d5245acc5a013bf880fae2d85124167ee
Udoka <udokakuugochukwu@gmail.com>
2022-12-02 05:03:24 +0100

    checking

---
+++ app.py
@@ -0,0 +1,19 @@
+import os, random, sys, time
+from flask import Flask, render_template, redirect, request
+
+from cs50 import SQL
+
+#configure application
+app = Flask(__name__)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///posts.db")
+
+#defaults requirements or stats
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
---
+++ modules.py
@@ -0,0 +1,4 @@
+import random, sys, werkzeug, os, string
+from csv import DictReader
+
+#file that will hold all pre written modules for the humanity web 
