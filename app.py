440b8837a1dc99460947943daaee0a96bfce714f
Udoka <udokakuugochukwu@gmail.com>
2022-12-04 04:28:07 +0100

    my own git account

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 import os, random, sys, time
 from flask import Flask, render_template, redirect, request
-
+ 
 from cs50 import SQL
+
+from modules import filter
 
 
 #configure application
 app = Flask(__name__)
 
+#ensure templates are auto reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///posts.db")
+db = SQL("sqlite:///humanity.db")
+
 
 #defaults requirements or stats
 @app.after_request
@@ -19,6 +27,16 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-@app.route("/", methods=["GET", "POST"])
-def help():
-    return
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
--- modules.py
+++ modules.py
@@ -2,3 +2,29 @@ import random, sys, werkzeug, os, string
 from csv import DictReader
 
 #file that will hold all pre written modules for the humanity web 
+
+#filter words synonymes 
+def filter(text):
+    lists= []
+    word = ""
+
+    for x in range(0,len(text)):
+        def tryer(text):
+            try:
+                text[x + 1]
+            except IndexError:
+                return True
+            return False
+        if text[x] == " ":
+            lists.append(word)
+            word = ""
+            continue
+        elif tryer(text) == True:
+            word += text[x]
+            lists.append(word)
+            word = ""
+            continue
+        word += text[x]
+    #print(lists)
+    return lists
+#done filtering words 
